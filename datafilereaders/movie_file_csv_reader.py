1c78acb028015e07066a49b54106503c7db73fb6
legalrick2 <lego.stickman@gmail.com>
2020-09-03 22:25:56 +1200

    Implemented the reader. take caution on line 18

--- datafilereaders/movie_file_csv_reader.py
+++ datafilereaders/movie_file_csv_reader.py
@@ -9,14 +9,73 @@ class MovieFileCSVReader:
 
     def __init__(self, file_name: str):
         self.__file_name = file_name
+        self.__dataset_of_movies = []
+        self.__dataset_of_actors = set([])
+        self.__dataset_of_directors = set([])
+        self.__dataset_of_genres = set([])
 
     def read_csv_file(self):
-        with open(self.__file_name, mode='r', encoding='utf-8-sig') as csvfile:
+        with open("datafiles/"+self.__file_name, mode='r', encoding='utf-8-sig') as csvfile: #i delete "datafiles/"+ before posting to coderunner, if this isnt here then my IDE, vscode, doesnt find it
             movie_file_reader = csv.DictReader(csvfile)
 
             index = 0
             for row in movie_file_reader:
-                title = row['Title']
-                release_year = int(row['Year'])
-                print(f"Movie {index} with title: {title}, release year {release_year}")
+                if Movie(row['Title'],int(row['Year'])) not in self.__dataset_of_movies:
+                    self.__dataset_of_movies.append(Movie(row['Title'], int(row['Year'])))
+                #description = row['Description']
+
+                self.__dataset_of_directors.add(Director(row['Director']))
+                self.dataset_of_movies[-1].director = Director(row['Director'])
+
+                for g in row['Genre'].split(","):
+                    self.__dataset_of_genres.add(Genre(g.strip()))
+
+                for a in row['Actors'].split(","):
+                    self.__dataset_of_actors.add(Actor(a.strip()))
                 index += 1
+                #print(f"Movie {index} with title: {title}, release year {release_year}")
+
+    @property
+    def dataset_of_movies(self):
+        return self.__dataset_of_movies
+    # @dataset_of_movies.setter
+    # def dataset_of_movies(self, li):
+    #     self._dataset_of_movies = li
+
+    @property
+    def dataset_of_actors(self):
+        return self.__dataset_of_actors
+    # @dataset_of_actors.setter
+    # def dataset_of_actors(self, li):
+    #     self._dataset_of_actors = li
+
+    @property
+    def dataset_of_directors(self):
+        return self.__dataset_of_directors
+    # @dataset_of_directors.setter
+    # def dataset_of_directors(self, li):
+    #     self._dataset_of_directors = li
+
+    @property
+    def dataset_of_genres(self):
+        return self.__dataset_of_genres
+    # @dataset_of_genres.setter
+    # def dataset_of_genres(self, li):
+    #     self._dataset_of_genres = li
+
+	
+	
+	
+# filename = 'Data1000Movies.csv'
+# movie_file_reader = MovieFileCSVReader(filename)
+# movie_file_reader.read_csv_file()
+
+# print(f'number of unique movies: {len(movie_file_reader.dataset_of_movies)}')
+# print(f'number of unique actors: {len(movie_file_reader.dataset_of_actors)}')
+# print(f'number of unique directors: {len(movie_file_reader.dataset_of_directors)}')
+# print(f'number of unique genres: {len(movie_file_reader.dataset_of_genres)}')
+
+# print(movie_file_reader.dataset_of_genres)
+# print(movie_file_reader.dataset_of_movies[1].director)
+
+#datafilereaders.movie_file_csv_reader
